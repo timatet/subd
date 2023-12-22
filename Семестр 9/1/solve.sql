111e3bfee94ce122b6195cca919d146ad470dfba
Tim <timofei.teterin@inbox.ru>
2023-12-22 10:19:58 +0300

    Update solve.sql

--- Семестр 9/1/solve.sql
+++ Семестр 9/1/solve.sql
@@ -1,10 +1,25 @@
-USE mask_test
+USE L1
+
+DROP TABLE dbo.Clearence
+
+CREATE TABLE Clearence
+(
+	level INT NOT NULL UNIQUE,
+	name NVARCHAR(50) PRIMARY KEY NOT NULL
+)
+
+INSERT INTO Clearence
+VALUES
+	(0, 'SECRET'),
+	(1, 'TOP SECRET'),
+	(2, 'UNCLASSIFIED')
 
 CREATE TABLE TestData
 (
 	id INT IDENTITY(1, 1) PRIMARY KEY,
 	name NVARCHAR(50) NOT NULL UNIQUE,
-	level NVARCHAR(50) NOT NULL
+	clearence NVARCHAR(50) NOT NULL
+	FOREIGN KEY(clearence) REFERENCES Clearence(name)
 )
 
 INSERT INTO TestData
@@ -16,15 +31,16 @@ VALUES
 CREATE TABLE UserData
 (
 	name NVARCHAR(50) NOT NULL PRIMARY KEY,
-	clearance NVARCHAR(50) NOT NULL,
-	level INT NOT NULL
+	clearence NVARCHAR(50) NOT NULL
+	FOREIGN KEY(clearence) REFERENCES Clearence(name)
 )
 
 INSERT INTO UserData
 VALUES
-	(N'Anna', 'SECRET', 1),
-	(N'Alex', 'UNCLASSIFIED', 2)
-	
+	(N'Anna', 'SECRET'),
+	(N'Alex', 'UNCLASSIFIED');
+
+-- Триггер на добавление новых пользователей в UserData
 CREATE OR ALTER TRIGGER UserRegisterTrigger
     ON DATABASE
     AFTER CREATE_USER
@@ -37,51 +53,59 @@ BEGIN
     	(@NewUser, 'UNCLASSIFIED') 
 END
 
+DELETE FROM UserData
+SELECT * FROM UserData
+
 CREATE ROLE [data_reader]
 GRANT SELECT ON TestData TO [data_reader]
 
 CREATE USER [Anna] WITHOUT LOGIN
-ALTER ROLE [data_reader] ADD MEMBER [Alex]
-
-CREATE USER [Alex] WITHOUT LOGIN
 ALTER ROLE [data_reader] ADD MEMBER [Anna]
 
+CREATE USER [Alex] WITHOUT LOGIN
+ALTER ROLE [data_reader] ADD MEMBER [Alex]
+
+UPDATE UserData
+SET clearence = 'SECRET'
+WHERE name = 'Alex'
+
+-- Уровень пользователя
+SELECT cl.level FROM UserData ud, Clearence cl WHERE ud.clearence = cl.name AND ud.name = 'Alex'
+
+-- Уровень секрета
+SELECT cl.level FROM Clearence cl WHERE cl.name = 'SECRET'
+
 CREATE SCHEMA UserSec
-CREATE OR ALTER FUNCTION [UserSec].[data_read]
-	(@Clearance AS NVARCHAR(20))
+
+-- Предикат управления доступом
+CREATE OR ALTER FUNCTION [UserSec].[data_reader_control](@clearence AS NVARCHAR(20))
 RETURNS TABLE
 WITH schemabinding
 AS
 RETURN
-    WITH 
-    cte_row_level AS (
-		SELECT TOP 1 
-    		C.level AS row_level
-		FROM [dbo].Classifications C
-		WHERE C.name = @clearance
-	),
-	cte_user_level AS (
-		SELECT TOP 1 
-			C.level AS user_level
-		FROM [dbo].Classifications C
-		JOIN [dbo].Users U ON C.name = U.clearance
-		WHERE U.name = CURRENT_USER
-	)
-    SELECT 1 AS fn_result
-    FROM cte_row_level, cte_user_level
-    WHERE cte_row_level.row_level >= cte_user_level.user_level
-    
-CREATE SECURITY policy [Security].P_RLS_DataTable_read
-ADD FILTER predicate [Security].[fn_DataTable_read]([level])
+    SELECT 1 AS allowed
+    WHERE (SELECT cl.level FROM dbo.Clearence cl WHERE cl.name = @clearence) 
+		>= (SELECT cl.level FROM dbo.UserData ud, dbo.Clearence cl WHERE ud.clearence = cl.name AND ud.name = CURRENT_USER) 
+
+CREATE SECURITY POLICY 
+	[UserSec].RLS_data_reader
+ADD FILTER PREDICATE [UserSec].[data_reader_control]([clearence])
 ON [dbo].[TestData]
-WITH (state=on)
+WITH (STATE=ON)
+
+DROP SECURITY POLICY [UserSec].RLS_data_reader
  
 -- Проверка
 EXECUTE AS USER = 'Anna'
-SELECT * FROM [dbo].[DataTable]
+SELECT * FROM dbo.TestData
 REVERT
  
 EXECUTE AS USER = 'Alex'
-SELECT * FROM [dbo].[DataTable]
+SELECT * FROM dbo.TestData
 REVERT
 
+SELECT CURRENT_USER
+
+INSERT INTO UserData
+VALUES
+	('dbo', 'SECRET')
