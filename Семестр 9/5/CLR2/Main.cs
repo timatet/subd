fe9de20c45bee3a0167b83d7cfe088d102091e04
Timofey Teterin <timofei.teterin@inbox.ru>
2023-12-24 00:21:24 +0300

    add lab 6 (only on windows sql server)

---
+++ Семестр 9/5/CLR2/Main.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.IO;
+
+public class LogFunctions
+{
+    [SqlFunction(FillRowMethodName = "LogInFile")]
+    public static string LogActionInFile(
+        string log_file,
+        DateTime date, 
+        string user, 
+        string object_type, 
+        string object_name, 
+        string sql
+    ) {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(log_file, true))
+            {
+                writer.WriteLineAsync(string.Format("{0},{1},{2},{3},{4}", date.ToString("G"), user, object_type, object_name, sql));
+            }
+        } catch(Exception e) {
+            return e.ToString();
+        }
+
+        return "1";
+    }
+}
--- Семестр 9/5/Задание 3.sql
+++ Семестр 9/5/Задание 3.sql
@@ -1,12 +1,33 @@
-CREATE TABLE ddl_log 
-(
-	AttemptDate DATE,
-	LoginName NVARCHAR(100),
-	DBName NVARCHAR(100),
-	SchemaName NVARCHAR(100),
-	ObjectName NVARCHAR(100),
-	TSQLCommand NVARCHAR(MAX)
-)
+CREATE DATABASE L6;
+USE L6;
+
+-- Настройки clr
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'clr enabled', 1;
+RECONFIGURE;
+
+ALTER DATABASE L6 SET TRUSTWORTHY ON;
+
+DROP FUNCTION IF EXISTS dbo.TestFunction;
+DROP ASSEMBLY IF EXISTS DotNetFunction;
+
+CREATE ASSEMBLY DotNetFunction 
+FROM '/.../CLR2.dll'
+WITH PERMISSION_SET = EXTERNAL_ACCESS;
+
+CREATE OR ALTER FUNCTION dbo.TestFunction(
+	@log_file NVARCHAR(100),
+	@date DATE, 
+	@user NVARCHAR(50), 
+	@object_type NVARCHAR(50), 
+	@object_name NVARCHAR(50), 
+	@sql NVARCHAR(max)
+) RETURNS NVARCHAR(max)
+AS EXTERNAL NAME DotNetFunction.LogFunctions.LogActionInFile;
+
+-- Проверка
+SELECT dbo.TestFunction('/.../test_file.csv', GETDATE(), 'test_user', 'obj_t_test', 'obj_n_test', 'sql_test');
 
 -- Аудит действий над функциями и процедурами
 CREATE OR ALTER TRIGGER audit_objects
@@ -31,9 +52,7 @@ BEGIN
 		= @Data.value('(/EVENT_INSTANCE/ObjectName)[1]', 'NVARCHAR(100)')
 	DECLARE @TSQLCommand NVARCHAR(MAX) 
 		= @Data.value('(/EVENT_INSTANCE/TSQLCommand/CommandText)[1]', 'NVARCHAR(MAX)')
-	INSERT INTO ddl_log 
-	VALUES
-	(GETDATE(), @LoginName, @DBName, @SchemaName, @ObjectName, @TSQLCommand)
+	-- ...
 END
 
 DROP PROCEDURE IF EXISTS TestAudit
@@ -41,5 +60,3 @@ CREATE OR ALTER PROCEDURE TestAudit AS
 BEGIN
     SELECT 'TestAudit'
 END
-
-SELECT * FROM dbo.ddl_log
