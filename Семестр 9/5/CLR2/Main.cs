e6f54ef7fbd67d219ccb28670619b6e77a702865
Timofei Teterin <timofei.teterin@inbox.ru>
2023-12-25 08:00:49 +0300

    clr update

--- Семестр 9/5/CLR2/Main.cs
+++ Семестр 9/5/CLR2/Main.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 public class LogFunctions
 {
@@ -17,7 +18,7 @@ public class LogFunctions
         {
             using (StreamWriter writer = new StreamWriter(log_file, true))
             {
-                writer.WriteLineAsync(string.Format("{0},{1},{2},{3},{4}", date.ToString("G"), user, object_type, object_name, sql).Replace('\n', ' '));
+                writer.WriteLineAsync(Regex.Replace(string.Format("{0},{1},{2},{3},{4}", date.ToString("G"), user, object_type, object_name, sql), @"\t|\n|\r", ""));
             }
         } catch(Exception e) {
             return e.ToString();
